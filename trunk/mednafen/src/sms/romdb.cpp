70af734da5a094061d22b459fce9ed8bfbc9b7ad
raz0red@gmail.com <raz0red@gmail.com@cda338fb-29fa-c121-542f-ec33efa4575d>
2011-12-22 07:16:08 +0000

    The following SMS/GG games now load correctly:   Fantastic Dizzy (0xc888222b)   Micro Machines (0xf7c524f6)   Excellent Dizzy (0xAA140C9C)

--- trunk/mednafen/src/sms/romdb.cpp
+++ trunk/mednafen/src/sms/romdb.cpp
@@ -5,18 +5,23 @@ namespace MDFN_IEN_SMS
 {
 
 static const rominfo_t game_list[] = {
-    {0x29822980, MAPPER_CODIES, DISPLAY_PAL, TERRITORY_EXPORT, -1, "Cosmic Spacehead"},
-    {0xB9664AE1, MAPPER_CODIES, DISPLAY_PAL, TERRITORY_EXPORT, -1, "Fantastic Dizzy"},
+    {0x29822980, MAPPER_CODIES, DISPLAY_PAL, TERRITORY_EXPORT, -1, "Cosmic Spacehead"},    
     {0xA577CE46, MAPPER_CODIES, DISPLAY_PAL, TERRITORY_EXPORT, -1, "Micro Machines"},
+    {0xB9664AE1, MAPPER_CODIES, DISPLAY_PAL, TERRITORY_EXPORT, -1, "Fantastic Dizzy"},
     {0x8813514B, MAPPER_CODIES, DISPLAY_PAL, TERRITORY_EXPORT, -1, "Excellent Dizzy (Proto)"},
+#ifdef WII
+    {0xAA140C9C, MAPPER_CODIES, DISPLAY_PAL, TERRITORY_EXPORT, CONSOLE_SMS, "Excellent Dizzy (Proto - GG)"}, 
+#else
     {0xAA140C9C, MAPPER_CODIES, DISPLAY_PAL, TERRITORY_EXPORT, -1, "Excellent Dizzy (Proto - GG)"}, 
+#endif
 
-    // Game Gear
+    // Game Gear    
     {0xd9a7f170, MAPPER_CODIES, DISPLAY_NTSC, TERRITORY_EXPORT, -1, "S.S. Lucifer" },
-
     {0x5e53c7f7, MAPPER_CODIES, DISPLAY_NTSC, TERRITORY_EXPORT, -1, "Ernie Els Golf" },
-    {0xc888222b, MAPPER_CODIES, DISPLAY_NTSC, TERRITORY_EXPORT, -1 /*CONSOLE_SMS*/, "Fantastic Dizzy" },
-
+    {0xc888222b, MAPPER_CODIES, DISPLAY_NTSC, TERRITORY_EXPORT, CONSOLE_SMS, "Fantastic Dizzy" },
+#ifdef WII
+    {0xf7c524f6, MAPPER_CODIES, DISPLAY_PAL, TERRITORY_EXPORT, -1, "Micro Machines"},     
+#endif
 
     // SG-1000
     { 0x092f29d6, MAPPER_CASTLE, DISPLAY_NTSC, TERRITORY_DOMESTIC, -1,	"The Castle" },
