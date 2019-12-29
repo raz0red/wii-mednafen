482d3a7f8c23d27adfae6140e24142b26beb8dac
raz0red <raz0red@gmail.com>
2019-12-29 15:33:56 -0800

    Added code comments

--- src/wii/emulator/Emulator.cpp
+++ src/wii/emulator/Emulator.cpp
@@ -1,323 +1,257 @@
-#include "main.h"
 #include "Emulator.h"
 #include "DatabaseManager.h"
+#include "main.h"
 #include "wii_mednafen.h"
 #include "wii_mednafen_main.h"
 
 #ifdef WII_NETTRACE
-#include <network.h> 
-#include "net_print.h"  
+#include <network.h>
+#include "net_print.h"
 #endif
-  
-extern volatile MDFN_Rect *VTDRReady;
-extern volatile MDFN_Surface *VTReady;
-extern MDFNGI *MDFNGameInfo;
+
+extern volatile MDFN_Rect* VTDRReady;
+extern volatile MDFN_Surface* VTReady;
+extern MDFNGI* MDFNGameInfo;
 
 extern uint32 round_up_pow2(uint32 v);
 
-extern "C" 
-{
-void WII_SetRotation( int rot ); 
+extern "C" {
+void WII_SetRotation(int rot);
 void WII_ChangeSquare(int xscale, int yscale, int xshift, int yshift);
 }
 
-Emulator::Emulator( const char* key, const char* name ) : 
-  m_key( key ),
-  m_name( name ),
-  m_frameSkip( true )
-{
-  memset( m_padData, 0, sizeof(u32)<<2 );
-  m_doubleStrike = DOUBLE_STRIKE_DEFAULT;
-  m_volume = VOLUME_DEFAULT;
+Emulator::Emulator(const char* key, const char* name)
+    : m_key(key), m_name(name), m_frameSkip(true) {
+    memset(m_padData, 0, sizeof(u32) << 2);
+    m_doubleStrike = DOUBLE_STRIKE_DEFAULT;
+    m_volume = VOLUME_DEFAULT;
 }
 
-Rect* Emulator::getScreenSize()
-{
-  return &m_screenSize;
+Rect* Emulator::getScreenSize() {
+    return &m_screenSize;
 }
 
-Rect* Emulator::getRotatedScreenSize()
-{
-  return &m_rotatedScreenSize;
+Rect* Emulator::getRotatedScreenSize() {
+    return &m_rotatedScreenSize;
 }
 
-Rect* Emulator::getEmulatorScreenSize()
-{
-  return &m_emulatorScreenSize;
+Rect* Emulator::getEmulatorScreenSize() {
+    return &m_emulatorScreenSize;
 }
 
-const char* Emulator::getKey()
-{
-  return m_key;
+const char* Emulator::getKey() {
+    return m_key;
 }
 
-const char* Emulator::getName()
-{
-  return m_name;
+const char* Emulator::getName() {
+    return m_name;
 }
 
-void Emulator::onPostLoad()
-{
-}
+void Emulator::onPostLoad() {}
 
-bool Emulator::onShowControlsScreen()
-{
-  dbEntry* entry = getDbManager().getEntry();
-  if( entry->wiimoteSupported )
-  {
-    // Wiimote is supported 
-    return true;
-  }
-
-  return wii_mednafen_show_controls_screen();
-}
-
-void Emulator::onPreLoop() 
-{
-}
-
-bool Emulator::updateDebugText( 
-  char* output, const char* defaultOutput, int len )
-{
-  return false;
-}
-
-bool Emulator::isRotationSupported()
-{
-  return false;
-}
-
-int Emulator::getRotation()
-{
-  return MDFN_ROTATE0;
-}
-
-void Emulator::getCurrentScreenSizeRatio( float* ratiox, float* ratioy )
-{   
-  *ratiox = (float)MDFNGameInfo->nominal_width/(float)VTDRReady->w;
-  *ratioy = (float)MDFNGameInfo->nominal_height/(float)VTDRReady->h;
-}
-
-void Emulator::getResizeScreenRect( Rect* rect )
-{ 
-  float ratiox, ratioy;
-  getCurrentScreenSizeRatio( &ratiox, &ratioy );
-
-  if( isDoubleStrikeEnabled() )
-  {
-    const ScreenSize* size = 
-      getRotation() ? 
-        getDoubleStrikeRotatedScreenSize() :
-        getDoubleStrikeScreenSize();
-      
-    rect->w = (float)size->r.w * ratiox;
-    rect->h = (float)size->r.h * ratioy; 
-  }
-  else
-  {
-    if( getRotation() )
-    {
-      rect->w = (float)m_rotatedScreenSize.w * ratiox;
-      rect->h = (float)m_rotatedScreenSize.h * ratioy; 
+bool Emulator::onShowControlsScreen() {
+    dbEntry* entry = getDbManager().getEntry();
+    if (entry->wiimoteSupported) {
+        // Wiimote is supported
+        return true;
     }
-    else
-    {
-      rect->w = (float)m_screenSize.w * ratiox;
-      rect->h = (float)m_screenSize.h * ratioy; 
+
+    return wii_mednafen_show_controls_screen();
+}
+
+void Emulator::onPreLoop() {}
+
+bool Emulator::updateDebugText(char* output,
+                               const char* defaultOutput,
+                               int len) {
+    return false;
+}
+
+bool Emulator::isRotationSupported() {
+    return false;
+}
+
+int Emulator::getRotation() {
+    return MDFN_ROTATE0;
+}
+
+void Emulator::getCurrentScreenSizeRatio(float* ratiox, float* ratioy) {
+    *ratiox = (float)MDFNGameInfo->nominal_width / (float)VTDRReady->w;
+    *ratioy = (float)MDFNGameInfo->nominal_height / (float)VTDRReady->h;
+}
+
+void Emulator::getResizeScreenRect(Rect* rect) {
+    float ratiox, ratioy;
+    getCurrentScreenSizeRatio(&ratiox, &ratioy);
+
+    if (isDoubleStrikeEnabled()) {
+        const ScreenSize* size = getRotation()
+                                     ? getDoubleStrikeRotatedScreenSize()
+                                     : getDoubleStrikeScreenSize();
+
+        rect->w = (float)size->r.w * ratiox;
+        rect->h = (float)size->r.h * ratioy;
+    } else {
+        if (getRotation()) {
+            rect->w = (float)m_rotatedScreenSize.w * ratiox;
+            rect->h = (float)m_rotatedScreenSize.h * ratioy;
+        } else {
+            rect->w = (float)m_screenSize.w * ratiox;
+            rect->h = (float)m_screenSize.h * ratioy;
+        }
     }
-  }
 #ifdef WII_NETTRACE
-    net_print_string( NULL, 0, "resizing to: w:%dx%d, ratio:%f,%f, rect->w:%d, rect->h:%d\n", 
-  VTDRReady->w, VTDRReady->h, ratiox, ratioy, rect->w, rect->h );
+    net_print_string(
+        NULL, 0, "resizing to: w:%dx%d, ratio:%f,%f, rect->w:%d, rect->h:%d\n",
+        VTDRReady->w, VTDRReady->h, ratiox, ratioy, rect->w, rect->h);
 #endif
 
-  if (wii_16_9_correction == WS_AUTO ? is_widescreen : wii_16_9_correction)
-  {
-    if( getRotation() )
-    {
-      rect->h = ( rect->h * 3 ) / 4;
+    if (wii_16_9_correction == WS_AUTO ? is_widescreen : wii_16_9_correction) {
+        if (getRotation()) {
+            rect->h = (rect->h * 3) / 4;
+        } else {
+            rect->w = (rect->w * 3) / 4;
+        }
     }
-    else
-    {
-      rect->w = ( rect->w * 3 ) / 4;
-    }
-  }
 
-  rect->w = ((rect->w+1)&~1);
-  rect->h = ((rect->h+1)&~1);
+    rect->w = ((rect->w + 1) & ~1);
+    rect->h = ((rect->h + 1) & ~1);
 }
 
-void Emulator::resizeScreen() 
-{
-  Rect r;
-  getResizeScreenRect( &r );
-  WII_ChangeSquare( r.w, r.h, 0, 0 );
+void Emulator::resizeScreen() {
+    Rect r;
+    getResizeScreenRect(&r);
+    WII_ChangeSquare(r.w, r.h, 0, 0);
 #ifdef WII_NETTRACE
-  net_print_string( NULL, 0, 
-    "resizeScreen, change square: %dx%d\n", r.w, r.h );
-#endif 
+    net_print_string(NULL, 0, "resizeScreen, change square: %dx%d\n", r.w, r.h);
+#endif
 }
 
-bool Emulator::getFrameSkip()
-{
-  return m_frameSkip;
+bool Emulator::getFrameSkip() {
+    return m_frameSkip;
 }
 
-void Emulator::setFrameSkip( bool skip )
-{
-  m_frameSkip = skip;
-}
- 
-bool Emulator::getAppliedFrameSkip()
-{
-   dbEntry* entry = getDbManager().getEntry();
-   if( entry->frameSkip == FRAME_SKIP_DEFAULT )
-   {
-     return getFrameSkip();
-   }
-   
-   return entry->frameSkip == FRAME_SKIP_ENABLED;
+void Emulator::setFrameSkip(bool skip) {
+    m_frameSkip = skip;
 }
 
-bool Emulator::isDoubleStrikeSupported()
-{
-  return true; 
-}
-   
-u8 Emulator::getBpp()
-{ 
-  return 32;
-}
-
-const ScreenSize* Emulator::getDefaultScreenSizes()
-{
-  return NULL;
-}
-
-int Emulator::getDefaultScreenSizesCount()
-{
-  return 0;
-}
-const char* Emulator::getScreenSizeName()
-{
-  return getScreenSizeName( m_screenSize.w, m_screenSize.h );
-}
-
-const char* Emulator::getScreenSizeName( int w, int h )
-{
-  const ScreenSize* sizes = getDefaultScreenSizes();
-  for( int i = 0; i < getDefaultScreenSizesCount(); i++ )
-  {
-    if( sizes[i].r.w == w && sizes[i].r.h == h )
-    {
-      return sizes[i].name;
+bool Emulator::getAppliedFrameSkip() {
+    dbEntry* entry = getDbManager().getEntry();
+    if (entry->frameSkip == FRAME_SKIP_DEFAULT) {
+        return getFrameSkip();
     }
-  }
 
-  return "Custom";
+    return entry->frameSkip == FRAME_SKIP_ENABLED;
 }
 
-const ScreenSize* Emulator::getDefaultRotatedScreenSizes()
-{
-  return NULL;
+bool Emulator::isDoubleStrikeSupported() {
+    return true;
 }
 
-int Emulator::getDefaultRotatedScreenSizesCount()
-{
-  return 0;
+u8 Emulator::getBpp() {
+    return 32;
 }
 
-const char* Emulator::getRotatedScreenSizeName()
-{
-  return getRotatedScreenSizeName( 
-    m_rotatedScreenSize.w, m_rotatedScreenSize.h );
+const ScreenSize* Emulator::getDefaultScreenSizes() {
+    return NULL;
 }
 
-const char* Emulator::getRotatedScreenSizeName( int w, int h )
-{
-  const ScreenSize* sizes = getDefaultRotatedScreenSizes();
-  for( int i = 0; i < getDefaultRotatedScreenSizesCount(); i++ )
-  {
-    if( sizes[i].r.w == w && sizes[i].r.h == h )
-    {
-      return sizes[i].name;
+int Emulator::getDefaultScreenSizesCount() {
+    return 0;
+}
+const char* Emulator::getScreenSizeName() {
+    return getScreenSizeName(m_screenSize.w, m_screenSize.h);
+}
+
+const char* Emulator::getScreenSizeName(int w, int h) {
+    const ScreenSize* sizes = getDefaultScreenSizes();
+    for (int i = 0; i < getDefaultScreenSizesCount(); i++) {
+        if (sizes[i].r.w == w && sizes[i].r.h == h) {
+            return sizes[i].name;
+        }
     }
-  }
 
-  return "Custom";
+    return "Custom";
 }
 
-const ScreenSize* Emulator::getDoubleStrikeScreenSize()
-{
-  return NULL;
+const ScreenSize* Emulator::getDefaultRotatedScreenSizes() {
+    return NULL;
 }
 
-const ScreenSize* Emulator::getDoubleStrikeRotatedScreenSize()
-{
-  return NULL;
+int Emulator::getDefaultRotatedScreenSizesCount() {
+    return 0;
 }
 
-void Emulator::setDoubleStrikeMode( int mode )
-{
-  m_doubleStrike = mode;  
+const char* Emulator::getRotatedScreenSizeName() {
+    return getRotatedScreenSizeName(m_rotatedScreenSize.w,
+                                    m_rotatedScreenSize.h);
 }
 
-int Emulator::getDoubleStrikeMode()
-{
-  return m_doubleStrike;
+const char* Emulator::getRotatedScreenSizeName(int w, int h) {
+    const ScreenSize* sizes = getDefaultRotatedScreenSizes();
+    for (int i = 0; i < getDefaultRotatedScreenSizesCount(); i++) {
+        if (sizes[i].r.w == w && sizes[i].r.h == h) {
+            return sizes[i].name;
+        }
+    }
+
+    return "Custom";
 }
 
-bool Emulator::isDoubleStrikeEnabled()
-{
-  return isDoubleStrikeSupported() && 
-    ( ( m_doubleStrike == DOUBLE_STRIKE_ENABLED ) ||
-      ( ( m_doubleStrike == DOUBLE_STRIKE_DEFAULT ) &&    
-        wii_double_strike_mode ) );
+const ScreenSize* Emulator::getDoubleStrikeScreenSize() {
+    return NULL;
 }
 
-int Emulator::getVolume()
-{
-  return m_volume;
-}
-void Emulator::setVolume( int volume )
-{
-  m_volume = volume;
+const ScreenSize* Emulator::getDoubleStrikeRotatedScreenSize() {
+    return NULL;
 }
 
-int Emulator::getAppliedVolume()
-{
-  if( m_volume == VOLUME_DEFAULT )
-  {
-    return wii_volume;
-  }
-  else
-  {
+void Emulator::setDoubleStrikeMode(int mode) {
+    m_doubleStrike = mode;
+}
+
+int Emulator::getDoubleStrikeMode() {
+    return m_doubleStrike;
+}
+
+bool Emulator::isDoubleStrikeEnabled() {
+    return isDoubleStrikeSupported() &&
+           ((m_doubleStrike == DOUBLE_STRIKE_ENABLED) ||
+            ((m_doubleStrike == DOUBLE_STRIKE_DEFAULT) &&
+             wii_double_strike_mode));
+}
+
+int Emulator::getVolume() {
     return m_volume;
-  }
+}
+void Emulator::setVolume(int volume) {
+    m_volume = volume;
 }
 
-bool Emulator::isRewindSupported()
-{
-  return false;
-}
-
-const char** Emulator::getInputDevices()
-{
-  return NULL;
-}
-
-void Emulator::updateInputDeviceData( int device, u8 *data, int size )
-{
-  if( device < 4 )
-  {
-    u16 padData = m_padData[device];
-    if( size >= 1 )
-    {
-      data[0] = padData & 0xFF;
+int Emulator::getAppliedVolume() {
+    if (m_volume == VOLUME_DEFAULT) {
+        return wii_volume;
+    } else {
+        return m_volume;
     }
-    if( size >= 2 )
-    {
-      data[1] = ( padData >> 8 ) & 0xFF;    
+}
+
+bool Emulator::isRewindSupported() {
+    return false;
+}
+
+const char** Emulator::getInputDevices() {
+    return NULL;
+}
+
+void Emulator::updateInputDeviceData(int device, u8* data, int size) {
+    if (device < 4) {
+        u16 padData = m_padData[device];
+        if (size >= 1) {
+            data[0] = padData & 0xFF;
+        }
+        if (size >= 2) {
+            data[1] = (padData >> 8) & 0xFF;
+        }
     }
-  }
 }
--- src/wii/emulator/Emulator.h
+++ src/wii/emulator/Emulator.h
@@ -13,18 +13,28 @@ class ConfigManager;
 class DatabaseManager;
 class MenuManager;
 
+/**
+ * Rectangle (used to specify width and height)
+ */
 typedef struct Rect {
-  u16 w;
-  u16 h;
+    /** The width */
+    u16 w;
+    /** The height */
+    u16 h;
 } Rect;
 
+/**
+ * Represents a specific screen size
+ */
 typedef struct ScreenSize {
-  Rect r;
-  const char* name;
+    /** The screen size */
+    Rect r;
+    /** The name of the specific screen size (2x, etc.) */
+    const char* name;
 } ScreenSize;
 
-#define BTN_RAPID   0x80000000
-#define BTN_REWIND  0x40000000
+#define BTN_RAPID 0x80000000
+#define BTN_REWIND 0x40000000
 
 #define DOUBLE_STRIKE_DISABLED 0
 #define DOUBLE_STRIKE_ENABLED 1
@@ -32,76 +42,352 @@ typedef struct ScreenSize {
 
 #define VOLUME_DEFAULT 160
 
-class Emulator
-{
+/**
+ * Base class for the various emulators supported by WiiMednafen.
+ * This class and its sub-classes attempt to encapsulate the features that
+ * specific to each emulator.
+ */
+class Emulator {
 private:
-  const char* m_key; 
-  const char* m_name;
-  bool m_frameSkip;
+    /** The key associated with the specific emulator in the map of emulators */
+    const char* m_key;
+    /** The name of the emulator */
+    const char* m_name;
+    /** Whether frame skip is enabled */
+    bool m_frameSkip;
 
 protected:
-  u16 m_padData[4];  
-  Rect m_emulatorScreenSize;
-  Rect m_screenSize;
-  Rect m_rotatedScreenSize;
-  int m_doubleStrike;
-  int m_volume;
+    /** Status of the various game pads */
+    u16 m_padData[4];
+    /** The size of the emulator screen (size of the backing surface) */
+    Rect m_emulatorScreenSize;
+    /** The user-specified screen size */
+    Rect m_screenSize;
+    /** The user-specified screen size when rotated */
+    Rect m_rotatedScreenSize;
+    /** Current double strike mode (enabled, disabled, default, etc.) */
+    int m_doubleStrike;
+    /** The volume */
+    int m_volume;
 
-  Emulator( const char* key, const char* name );
+    /**
+     * Constructs the emulator
+     *
+     * @param   key The key associated with the specific emulator in the map of
+     *          emulators
+     * @param   name The name of the emulator
+     */
+    Emulator(const char* key, const char* name);
 
-  virtual const ScreenSize* getDoubleStrikeScreenSize();
-  virtual const ScreenSize* getDoubleStrikeRotatedScreenSize();
+    /**
+     * The screen size of the emulator in double strike mode
+     *
+     * @return  The screen size of the emulator in double strike mode
+     */
+    virtual const ScreenSize* getDoubleStrikeScreenSize();
+
+    /**
+     * The screen size of the emulator in double strike mode when rotated
+     *
+     * @return  The screen size of the emulator in double strike mode when
+     *          rotated
+     */
+    virtual const ScreenSize* getDoubleStrikeRotatedScreenSize();
 
 public:
-  const char* getKey();
-  const char* getName();
-  virtual ConfigManager& getConfigManager() = 0;
-  virtual DatabaseManager& getDbManager() = 0;
-  virtual MenuManager& getMenuManager() = 0;
-  virtual void updateControls( bool isRapid ) = 0;
-  virtual void onPostLoad();
-  virtual bool onShowControlsScreen();
-  virtual void onPreLoop();
-  virtual bool updateDebugText( 
-    char* output, const char* defaultOutput, int len );
-  virtual bool isRotationSupported();
-  virtual int getRotation();
-  virtual u8 getBpp();
-  virtual void resizeScreen();
-  void getResizeScreenRect( Rect* rect );
-  void getCurrentScreenSizeRatio( float* ratiox, float* ratioy );
- 
-  virtual const ScreenSize* getDefaultScreenSizes();
-  virtual int getDefaultScreenSizesCount();
-  const char* getScreenSizeName();
-  const char* getScreenSizeName( int w, int h );
+    /**
+     * Returns the key associated with the specific emulator in the map of
+     * emulators
+     *
+     * @return  The key associated with the specific emulator in the map of
+     *          emulators
+     */
+    const char* getKey();
 
-  virtual const ScreenSize* getDefaultRotatedScreenSizes();
-  virtual int getDefaultRotatedScreenSizesCount();
-  const char* getRotatedScreenSizeName();
-  const char* getRotatedScreenSizeName( int w, int h );
+    /**
+     * Returns the name of the emulator 
+     *
+     * @return  The name of the emulator 
+     */
+    const char* getName();
 
-  virtual bool isDoubleStrikeSupported();
-  virtual bool isDoubleStrikeEnabled();
-  void setDoubleStrikeMode( int mode );
-  int getDoubleStrikeMode();
+    /**
+     * Returns the {@class ConfigManager} for the emulator
+     * 
+     * @return  The {@class ConfigManager} for the emulator
+     */
+    virtual ConfigManager& getConfigManager() = 0;
 
-  Rect* getEmulatorScreenSize();
-  Rect* getScreenSize();
-  Rect* getRotatedScreenSize();
+    /**
+     * Returns the {@class DatabaseManager} for the emulator
+     * 
+     * @return  The {@class DatabaseManager} for the emulator
+     */
+    virtual DatabaseManager& getDbManager() = 0;
 
-  virtual bool isRewindSupported();
+    /**
+     * Returns the {@class MenuManager} for the emulator
+     * 
+     * @return  The {@class MenuManager} for the emulator
+     */
+    virtual MenuManager& getMenuManager() = 0;
 
-  int getVolume();
-  void setVolume( int volume );
-  int getAppliedVolume();
+    /**
+     * Updates the state of the controllers
+     * 
+     * @param   isRapid Whether rapid fire is occurring
+     */
+    virtual void updateControls(bool isRapid) = 0;
 
-  virtual void updateInputDeviceData( int device, u8 *data, int size );
-  virtual const char** getInputDevices();
-  
-  bool getFrameSkip();
-  void setFrameSkip( bool skip );
-  bool getAppliedFrameSkip();
+    /**
+     * Invoked after the current game is loaded
+     */
+    virtual void onPostLoad();
+
+    /**
+     * Gives emulator an opportunity to show a "controls" screen. displays 
+     * applicable controllers, etc.
+     * 
+     * @return  Whether a controls screen was displayed
+     */
+    virtual bool onShowControlsScreen();
+
+    /**
+     * Invoked prior to the emulator loop starting
+     */
+    virtual void onPreLoop();
+
+    /**
+     * Provides the emulator an opportunity to update the debug text
+     * 
+     * @param   output The final debug text
+     * @param   defaultOutput the default debug text
+     * @param   len The length of the output text
+     * @return  Whether the text was updated
+     */
+    virtual bool updateDebugText(char* output,
+                                 const char* defaultOutput,
+                                 int len);
+
+    /**
+     * Whether the emulator supports screen rotation
+     * 
+     * @return    Whether the emulator supports screen rotation
+     */
+    virtual bool isRotationSupported();
+
+    /**
+     * Returns the current rotation state of the emulator (MDFN_ROTATE0, etc.)
+     *
+     * @return    The current rotation state of the emulator (MDFN_ROTATE0,
+     *            etc.)
+     */
+    virtual int getRotation();
+    
+    /**
+     * Returns the bits per pixel (BPP) for the emulator
+     * 
+     * @return    the bits per pixel (BPP) for the emulator
+     */
+    virtual u8 getBpp();
+
+    /**
+     * Resizes the screen to match the screen size set by the user
+     */
+    virtual void resizeScreen();
+
+    /**
+     * Specifies how the screen should be resized
+     *
+     * @param   rect (out) The rect that contains how the screen should be
+     *          resized
+     */
+    void getResizeScreenRect(Rect* rect);
+
+    /**
+     * Returns the screen size ratio
+     * 
+     * @param   ratiox (out) The x ratio
+     * @param   ratioy (out) The y ratio
+     */
+    void getCurrentScreenSizeRatio(float* ratiox, float* ratioy);
+
+    /**
+     * Returns an array of default screen sizes
+     * 
+     * @return  The array of default screen sizes
+     */
+    virtual const ScreenSize* getDefaultScreenSizes();
+
+    /**
+     * Returns the count of default screen sizes
+     * 
+     * @return  The count of default screen sizes
+     */
+    virtual int getDefaultScreenSizesCount();
+
+    /**
+     * Returns the name of the current screen size
+     * 
+     * @return  The name of the current screen size
+     */
+    const char* getScreenSizeName();
+
+    /**
+     * Returns the name of the screen size with the specified dimensions
+     *
+     * @return    The name of the screen size with the specified dimensions
+     */
+    const char* getScreenSizeName(int w, int h);
+
+    /**
+     * Returns an array of default rotated screen sizes
+     * 
+     * @return  The array of default roteated screen sizes
+     */
+    virtual const ScreenSize* getDefaultRotatedScreenSizes();
+
+    /**
+     * Returns the count of default rotated screen sizes
+     * 
+     * @return  The count of default rotated screen sizes
+     */
+    virtual int getDefaultRotatedScreenSizesCount();
+
+    /**
+     * Returns the name of the current rotated screen size
+     * 
+     * @return  The name of the current rotated screen size
+     */
+    const char* getRotatedScreenSizeName();
+
+    /**
+     * Returns the name of the rotated screen size with the specified dimensions
+     *
+     * @return    The name of the rotated screen size with the specified
+     *            dimensions
+     */
+    const char* getRotatedScreenSizeName(int w, int h);
+
+    /**
+     * Returns whether the emulator supports double strike mode
+     * 
+     * @return  Whether the emulator supports double strike mode
+     */
+    virtual bool isDoubleStrikeSupported();
+
+    /**
+     * Returns whether double strike mode is enabled for the emulator
+     *
+     * @return  Whether double strike mode is enabled for the emulator
+     */
+    virtual bool isDoubleStrikeEnabled();
+
+    /**
+     * Sets the double strike mode for the emulator (enabled, disabled, default)
+     *
+     * @param   mode The double strike mode for the emulator (enabled, disabled,
+     *          default)
+     */
+    void setDoubleStrikeMode(int mode);
+
+    /**
+     * Returns the double strike mode for the emulator (enabled, disabled,
+     * default)
+     *
+     * @return  The double strike mode for the emulator (enabled, disabled,
+     *          default)
+     */
+    int getDoubleStrikeMode();
+
+    /**
+     * Returns the size of the emulator screen (size of the backing surface)
+     *
+     * @return  The size of the emulator screen (size of the backing surface)
+     */
+    Rect* getEmulatorScreenSize();
+
+    /**
+     * Returns the user-specified screen size 
+     * 
+     * @return  The user-specified screen size 
+     */
+    Rect* getScreenSize();
+
+    /**
+     * Returns the user-specified rotated screen size 
+     * 
+     * @return  The user-specified rotated screen size 
+     */
+    Rect* getRotatedScreenSize();
+
+    /**
+     * Returns whether rewind is supported by the emulator
+     * 
+     * @return  Whether rewind is supported by the emulator
+     */
+    virtual bool isRewindSupported();
+
+    /**
+     * Returns the volume for the emulator
+     * 
+     * @return  The volume for the emulator
+     */
+    int getVolume();
+
+    /**
+     * Sets the volume for the emulator
+     * 
+     * @param   volume The volume for the emulator
+     */
+    void setVolume(int volume);
+
+    /**
+     * Returns the applied volume for the emulator (considers default)
+     *
+     * @return  The applied volume for the emulator (considers default)
+     */
+    int getAppliedVolume();
+
+    /**
+     * Invoked by Mednafen to update the input device data 
+     *
+     * @param   device The device being updated
+     * @param   data The input device data
+     * @param   size The size of the input device data
+     */
+    virtual void updateInputDeviceData(int device, u8* data, int size);
+
+    /**
+     * Returns an array of strings indicating the current set of input devices
+     * for the emulator
+     *
+     * @return  An array of strings indicating the current set of input devices
+     *          for the emulator
+     */
+    virtual const char** getInputDevices();
+
+    /**
+     * Whether frame skip is enabled
+     * 
+     * @return  Whether frame skip is enabled
+     */
+    bool getFrameSkip();
+
+    /**
+     * Sets whether frame skip is enabled
+     * 
+     * @param   skip Whether frame skip is enabled
+     */
+    void setFrameSkip(bool skip);
+
+    /**
+     * Whether frame skip is enabled (considers setting for game from db)
+     *
+     * @return  Whether frame skip is enabled (considers setting for game from
+     *          db)
+     */
+    bool getAppliedFrameSkip();
 };
 
 #endif
